697b87eb7d223cdc890d463fcafccdd63b798bba
LudrahsGartage <101329074+LudrahsGartage@users.noreply.github.com>
2022-10-11 09:18:01 -0700

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BusinessCardContainer from './components/BusinessCardContainer'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BusinessCardContainer />
-  </React.StrictMode>
-)
